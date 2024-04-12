8d39fd98699cdaefaa5362425a9f822724cb96c5
euics <pjh575157@gmail.com>
2024-04-12 19:25:39 +0900

    Initial Commit

---
+++ src/test/java/com/example/publicdatabackend/PublicDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publicdatabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicDataBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
