aa27d9365a8484fa39de6dab00e8bef731979504
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-04-30 14:24:15 +0900

    refactor: 리뷰작성시 사진 s3에 업로드 되도록 수정

--- src/main/java/com/example/publicdatabackend/dto/review/NormalReviewDto.java
+++ src/main/java/com/example/publicdatabackend/dto/review/NormalReviewDto.java
@@ -1,12 +1,17 @@
 package com.example.publicdatabackend.dto.review;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @Data
 @SuperBuilder
 public class NormalReviewDto extends ReviewDto {
+    @JsonIgnore
+    private MultipartFile photoFile;
+
 }
